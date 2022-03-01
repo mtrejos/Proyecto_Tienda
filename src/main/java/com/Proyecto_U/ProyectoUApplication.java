8d991a93a5075085681f957edcb14209723023cd
maria <maria@LAPTOP-PAVILON>
2022-02-28 20:40:10 -0600

    Primer commit del proyecto tienda

---
+++ src/main/java/com/Proyecto_U/ProyectoUApplication.java
@@ -0,0 +1,13 @@
+package com.Proyecto_U;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoUApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoUApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Proyecto_U/ProyectoUApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Proyecto_U;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoUApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
